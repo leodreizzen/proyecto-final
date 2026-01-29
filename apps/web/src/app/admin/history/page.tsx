a3811e42e62fc6322a2b21401d9b79a43c36edf5
Leonardo Dreizzen <leodreizzen@gmail.com>
2026-01-29 02:56:50 +0000

    Fix authcheck

--- apps/web/src/app/admin/history/page.tsx
+++ apps/web/src/app/admin/history/page.tsx
@@ -1,9 +1,9 @@
-import {checkResourcePermission} from "@/lib/auth/data-authorization";
 import {fetchUnfinishedUploads, fetchUploadHistory} from "@/lib/data/uploads";
 import {HistoryView} from "@/components/admin/history-view";
+import {authCheck} from "@/lib/auth/route-authorization";
 
 export default async function HistoryPage() {
-    await checkResourcePermission("upload", "read");
+    await authCheck(["ADMIN"])
 
     const [history, pendingUploads] = await Promise.all([
         fetchUploadHistory(null),
